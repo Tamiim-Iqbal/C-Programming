24a9551d09932c2067b5585a1686d0be7c148671
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2023-10-24 20:18:03 +0600

    2

---
+++ Module 10.5 - W3 PD1/F_Way_Too_Long_Words.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while (t>0)
+    {
+        char a[101];
+        scanf("%s",a);
+        int s=strlen(a);
+        if (s<=10)
+        {
+            printf("%s\n",a);
+        }
+        else 
+        {
+            printf("%c%d%c\n",a[0],s-2,a[s-1]);
+        }
+        t--;
+    }
+    return 0;
+}
---
+++ Module 10.5 - W3 PD1/M_Replace_MinMax.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n];
+    int mn = INT_MAX;
+    int mx = INT_MIN;
+    int mn_ind,mx_ind;
+
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for (int i=0; i<n; i++)
+    {
+        if (ar[i]>mx)
+        {
+            mx = ar[i];
+            mx_ind = i;
+        }
+    }
+    for (int i=0; i<n; i++)
+    {
+        if (ar[i]<mn)
+        {
+            mn = ar[i];
+            mn_ind = i;
+        }
+    }
+    int tmp=mn;
+    ar[mn_ind]=mx;
+    ar[mx_ind]=tmp;
+
+    for (int i=0; i<n; i++)
+    {
+        printf("%d ",ar[i]);
+    }
+    return 0;
+}
